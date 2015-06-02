a9a4c84dcfe74ce8e211382edab0a45d8d5bcb0f
Quinton Aiken <quintonlaiken@gmail.com>
2015-06-02 16:01:42 -0500

    implement getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+  var nodeList = [];
+
+  var childLoop = function(parent) {
+    var children = parent.children;
+    var length = children.length;
+    var i = 0;
+    var classList;
+
+    if(!length) {
+      return;
+    }
+
+    for(; i < length; ++i) {
+      classList = [].slice.call(children[i].classList);
+
+      if( classList.indexOf(className) !== -1 ) {
+        nodeList.push(children[i]);
+      }
+      
+      childLoop(children[i]);
+    }
+  };
+
+  childLoop(document);
+
+  return nodeList;
 };
